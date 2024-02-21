dbf9746feb7abc36905a15eb09ca4e61202bd740
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2024-02-21 10:22:18 -0700

    restrict github terraform bot to single repo
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

--- github/github.tf
+++ github/github.tf
@@ -2,29 +2,33 @@ variable "github_org" {
   type = string
 }
 
-# resource "teleport_provision_token" "github_actions" {
-#   version = "v2"
-#   metadata = {
-#     name        = "github-actions-bot"
-#     description = "Token for GitHub Actions bot for CI Terraform runs"
-#   }
+variable "github_repo" {
+  type = string
+}
 
-#   spec = {
-#     roles = ["Bot"]
-#     bot_name = "github-actions"
-#     join_method = "github"
-#     github: {
-#       allow: [
-#         {
-#           "repository_owner": var.github_org
-#         }
-#       ]
-#     }
-#   }
-# }
+resource "teleport_provision_token" "github_actions" {
+  version = "v2"
+  metadata = {
+    name        = "github-actions-bot"
+    description = "Token for GitHub Actions bot for CI Terraform runs"
+  }
 
-# resource "teleport_bot" "github_actions" {
-#   name = "github-actions"
-#   token_id = teleport_provision_token.github_actions.metadata.name
-#   roles = ["terraform"]
-# }
+  spec = {
+    roles = ["Bot"]
+    bot_name = "github-actions"
+    join_method = "github"
+    github: {
+      allow: [
+        {
+          "repository": "${var.github_org}/${var.github_repo}"
+        }
+      ]
+    }
+  }
+}
+
+resource "teleport_bot" "github_actions" {
+  name = "github-actions"
+  token_id = teleport_provision_token.github_actions.metadata.name
+  roles = ["terraform"]
+}
